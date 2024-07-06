b6549bd71e15e60e7af6e1346ee8885f59972eed
SudhirAscend <sudhir@ascenddigitech.com>
2024-07-06 11:14:20 +0530

    Login removed

--- milira_ecommerce/resources/views/layouts/topbar.blade.php
+++ milira_ecommerce/resources/views/layouts/topbar.blade.php
@@ -563,7 +563,7 @@
               <div class="text-center">
                 <img src="https://placehold.co/110x110/png" class="rounded-circle p-1 shadow mb-3" width="90" height="90"
                   alt="">
-                <h5 class="user-name mb-0 fw-bold">Hello, {{ Auth::user()->name }}</h5>
+          
               </div>
             </a>
             <hr class="dropdown-divider">
@@ -580,7 +580,7 @@
             <hr class="dropdown-divider">
             <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:void(0);" onclick="document.getElementById('logout-form').submit()"><i
             class="material-icons-outlined">power_settings_new</i>Logout</a>
-            <form action="{{ route('logout') }}" method="POST" id="logout-form">
+
                 @csrf
             </form>
           </div>
--- milira_ecommerce/routes/web.php
+++ milira_ecommerce/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('{any}', [HomeController::class, 'root'])->where('any', '.*');
