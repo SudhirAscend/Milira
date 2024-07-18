ce1c7ae4c6e493c3f9830c9def5613dcff2a03df
vigneshe~ <vigneshchellamthu.com>
2024-07-18 22:51:25 +0530

    Changes updated

--- milira_ecommerce/app/Http/Controllers/HomeController.php
+++ milira_ecommerce/app/Http/Controllers/HomeController.php
@@ -3,32 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\ProductCategory;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-       // $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('index');
+        $categories = ProductCategory::all();
+        $products = Product::all();
+        return view('index', compact('categories', 'products'));
     }
 
     public function root(Request $request)
     {
-        if(view()->exists($request->path())) {
+        if (view()->exists($request->path())) {
             return view($request->path());
         } else {
             return abort(404);
