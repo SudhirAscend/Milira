ff64286084ec70a861b82dc6ab2c34d3eae0ba1a
vigneshe~ <vigneshchellamthu.com>
2024-07-25 12:36:26 +0530

    changes updated

--- milira_ecommerce/resources/views/shop.blade.php
+++ milira_ecommerce/resources/views/shop.blade.php
@@ -31,9 +31,16 @@
 .wishlist-button.wishlisted i {
     color: red;
 }
-
-
-
+.wishlist-count {
+    position: relative;
+    left: 40px;
+    top: -10px;
+    background-color: red;
+    color: white !important;
+    border-radius: 50%;
+    padding:1px 6px;
+    font-size: 10px;
+}
     .cart-count {
         position: absolute;
         top: -10px;
@@ -174,14 +181,16 @@
                         </div>
                         <div class="header-favourite">
     <a href="wishlist.html" class="cart-item">
+    <span class="wishlist-count"  id="wishlist-item-count">{{ $wishlistCount }}</span>
         <span>
+        
             <svg width="35" height="27" viewBox="0 0 35 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11.4047 8.54989C11.6187 8.30247 11.8069 8.07783 12.0027 7.86001C15.0697 4.45162 20.3879 5.51717 22.1581 9.60443C23.4189 12.5161 22.8485 15.213 20.9965 17.6962C19.6524 19.498 17.95 20.9437 16.2722 22.4108C15.0307 23.4964 13.774 24.5642 12.5246 25.6408C11.6986 26.3523 11.1108 26.3607 10.2924 25.6397C8.05177 23.6657 5.79225 21.7125 3.59029 19.6964C2.35865 18.5686 1.33266 17.2553 0.638823 15.7086C-0.626904 12.8872 0.0324709 9.41204 2.22306 7.41034C4.84011 5.01855 8.81757 5.36918 11.1059 8.19281C11.1968 8.30475 11.2907 8.41404 11.4047 8.54989Z" fill="#6E6D79" />
-                <circle cx="26.7662" cy="8" r="8" fill="#000" />
+                
+        
                 <path d="M26.846 13.1392C26.1632 13.1392 25.5534 13.0215 25.0164 12.7862C24.4828 12.5509 24.0602 12.2244 23.7487 11.8068C23.4404 11.3859 23.2747 10.8987 23.2515 10.3452H24.8126C24.8325 10.6468 24.9336 10.9086 25.1159 11.1307C25.3015 11.3494 25.5434 11.5185 25.8417 11.6378C26.14 11.7571 26.4715 11.8168 26.836 11.8168C27.2371 11.8168 27.5917 11.7472 27.9 11.608C28.2115 11.4687 28.4551 11.2749 28.6308 11.0263C28.8065 10.7744 28.8943 10.4844 28.8943 10.1562C28.8943 9.81487 28.8065 9.51491 28.6308 9.25639C28.4584 8.99455 28.2049 8.78906 27.8701 8.63991C27.5387 8.49077 27.1377 8.41619 26.667 8.41619H25.8069V7.16335H" fill="#F9FFFB" />
             </svg>
-            <span class="wishlist-count" id="wishlist-item-count">{{ $wishlistCount }}</span>
-        </span>
+            </span>
         <span class="cart-text">Wishlist</span>
     </a>
 </div>
@@ -1216,7 +1225,8 @@ $(document).ready(function() {
                             this.querySelector('i').classList.remove('bi-heart');
                             this.querySelector('i').classList.add('bi-heart-fill');
                         }
-                        document.getElementById('wishlist-count').innerText = data.wishlistCount;
+                        document.getElementById('wishlist-item-count').innerText = data.wishlistCount;
+                        document.getElementById('wishlist-circle').style.fill = data.wishlistCount > 0 ? 'red' : '#000';
                     }
                 });
             });
