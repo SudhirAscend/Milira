ef4af77f979013032f8404dda34f2796f50a1ae9
vigneshe~ <vigneshchellamthu.com>
2024-07-25 21:08:41 +0530

    Chnages updated

--- milira_ecommerce/resources/views/shop.blade.php
+++ milira_ecommerce/resources/views/shop.blade.php
@@ -207,11 +207,13 @@
                                 <p class="new-price">₹{{ $product['price'] }}</p>
                             </div>
                         </div>
+                        
                         <span class="close-btn" data-id="{{ $productId }}">
                             <svg viewBox="0 0 10 10" fill="none" class="fill-current" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z"></path>
                             </svg>
                         </span>
+                        
                     </div>
                 </div>
             @endforeach
