7c74b38aadc542e3406b5bf0fbe0352996fb5cca
vigneshe~ <vigneshchellamthu.com>
2024-07-25 17:01:05 +0530

    Change updated in header

--- milira_ecommerce/resources/views/shop.blade.php
+++ milira_ecommerce/resources/views/shop.blade.php
@@ -1102,8 +1102,8 @@ $(document).ready(function() {
                 success: function(response) {
                     alert(response.message);
                     $('#cart-item-count').text(response.cartCount);
-                    updateCartDetails(response.cart, response.subtotal);
                     updateCartProducts(response.cart);
+                    $('#cart-subtotal').text('₹' + response.subtotal);
                 },
                 error: function(response) {
                     if(response.status === 401) {
@@ -1116,15 +1116,11 @@ $(document).ready(function() {
             });
         });
 
-        function updateCartCount(cartCount) {
-            $('#cart-item-count').text(cartCount);
-        }
-
         function updateCartProducts(cart) {
             var cartItemsHtml = '';
-            var subtotal = 0;
             $.each(cart, function(productId, product) {
                 cartItemsHtml += `
+                <div class="wrapper">
                     <div class="wrapper-item">
                         <div class="wrapper-img">
                             <img src="assets/images/homepage-one/product-img/product-img-1.webp" alt="img">
@@ -1135,16 +1131,21 @@ $(document).ready(function() {
                                 <p class="new-price">₹${product.price * product.quantity}</p>
                             </div>
                         </div>
+                        <span class="close-btn">
+                            <svg viewBox="0 0 10 10" fill="none" class="fill-current" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z"></path>
+                            </svg>
+                        </span>
+                    </div>
                     </div>
                 `;
-                subtotal += product.price * product.quantity;
             });
-            $('.cart-wrapper-item').html(cartItemsHtml);
-            $('#cart-subtotal').text('₹' + subtotal);
+            $('#cart-wrapper-item').html(cartItemsHtml);
         }
     });
 </script>
 
+   
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
