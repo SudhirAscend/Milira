6d39fb43da943fde8eef1a2ea5bc3dfa33c56b48
viki T <viki123@gmail.com>
2024-07-26 14:25:53 +0530

    footer updated

---
+++ milira_ecommerce/resources/views/footer.blade.php
@@ -0,0 +1,145 @@
+<footer id="footer">
+        <div class="footer-details">
+            <div class="foot-logo py-5">
+                <img src="../assets/images/logos/Milira-White-Logo.png" alt="Milira-White-Logo">
+            </div>
+            <div class="footer-content">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                        <div class="foot-navigation">
+                            <div class="foot-title">
+                                <h4>Navigation</h4>
+                            </div>
+                            <div class="foot-items">
+                                <ul>
+                                    <li>
+                                        <a href="/index">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="/about">About</a>
+                                    </li>
+                                    <li>
+                                        <a href="/shop">Shop</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Categories</a>
+                                    </li>
+                                    <li>
+                                        <a href="/contact">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                        <div class="foot-navigation">
+                            <div class="foot-title">
+                                <h4>Categories</h4>
+                            </div>
+                            <div class="foot-items">
+                                <ul>
+                                    <li>
+                                        <a href="#">Pendemt Set</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Chain</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Stud</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Earrings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Bracelet</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Necklace</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                        <div class="foot-navigation">
+                            <div class="foot-title">
+                                <h4>Account</h4>
+                            </div>
+                            <div class="foot-items">
+                                <ul>
+                                    <li>
+                                        <a href="#">My Account</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Account Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Payments</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Subscription</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Dashboard</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                        <div class="foot-navigation">
+                            <div class="foot-title">
+                                <h4>Legal</h4>
+                            </div>
+                            <div class="foot-items">
+                                <ul>
+                                    <li>
+                                        <a href="#">Terms & Conditions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Privacy Policy</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shipping Policy</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Payment Policy</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Refund Policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyrights mt-4">
+                <div class="row pt-4">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                        <div class="copy">
+                            <p>©<span id="demo"></span> by milira, All rights reserved</p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                        <div class="social">
+                            <p>
+                                <span>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                </span>
+                                <span>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                </span>
+                                <span>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </span>
+                                <span>
+                                    <a href=""><i class="bi bi-youtube"></i></a>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
