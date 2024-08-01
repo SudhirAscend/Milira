fa4ab0c34ad33507bf482b81d904bcb737d43476
vigneshe~ <vigneshchellamthu.com>
2024-08-01 11:31:39 +0530

    Changes updated in home page

--- milira_ecommerce/app/Http/Controllers/CollectionController.php
+++ milira_ecommerce/app/Http/Controllers/CollectionController.php
@@ -11,25 +11,33 @@ class CollectionController extends Controller
     {
         return view('admin.collections.create');
     }
-
     public function store(Request $request)
     {
         $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:50000',
         ]);
-
+    
         $data = $request->all();
-
+    
         if ($request->hasFile('image')) {
-            $imageName = time().'.'.$request->image->extension();
+            // Sanitize the name to create a valid filename
+            $sanitizedFileName = preg_replace('/[^A-Za-z0-9\-]/', '_', $request->name);
+            
+            // Set the file name using the sanitized name and original file extension
+            $imageName = $sanitizedFileName . '.' . $request->image->extension();
+    
+            // Move the file to the desired location
             $request->image->move(public_path('images/collections'), $imageName);
+    
+            // Save the image name to the data array
             $data['image'] = $imageName;
         }
-
+    
         Collection::create($data);
-
+    
         return redirect()->route('collections.create')->with('success', 'Collection added successfully.');
     }
+    
 }
